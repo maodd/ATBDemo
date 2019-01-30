714cd4947834e2b1aef41af2fea1460839c5d8ee
Frank Mao <maodd.ca@gmail.com>
2019-01-30 15:32:22 +0800

    Initial Commit

---
+++ AtbDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AtbDemo
+//
+//  Created by Frank Mao on 2019-01-30.
+//  Copyright © 2019 mazoic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
